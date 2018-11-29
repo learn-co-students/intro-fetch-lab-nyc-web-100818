678a8a98d30b7e5ead369f41614a427d0afbeab2
skbtrc <skbsturac@live.com>
2018-11-29 14:18:12 -0500

    Done.

--- swapi.js
+++ swapi.js
@@ -1,1 +1,10 @@
 // Write your swapi code in this file!
+document.addEventListener('DOMContentLoaded', function(event) {
+  console.log('Swapi Content Loaded');
+
+  const getOpeningCrawlButton = document.querySelector('#crawlBtn')
+  const planetSearchFrom = document.querySelector('#planetForm')
+  const planetSearchInput = document.querySelector('#planetInput')
+  const planetDataDiv = document.querySelector('#planetData')
+  
+})
