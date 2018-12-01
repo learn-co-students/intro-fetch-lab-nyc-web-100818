15c184b155c47d43d6761c3350e28cc9a80cc588
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-12-01 12:38:31 -0500

    Done.

--- numbers.js
+++ numbers.js
@@ -5,21 +5,20 @@
 // fetch a random fact about the number 1 ✅
 // Add the fact to the DOM in the `#one-facts` div ✅
 
-//  Pick a Number, Any Number. ✅
-
+// Pick a Number, Any Number. ✅
 // When a user enters a number in the input:
 // On Change, fetch a math fact about that number ✅
 // Try adding a validation so that a user can 't submit a non-number ✅
 // Show it on the screen in the `#random-math-fact` div ✅
 
 // Those who fail to study history are doomed to repeat it
-//When the page loads, start an interval:
-// Every 5 seconds, fetch a fact about a year and show it on the screen in the `#year-history` div
-// Start with this year *
-// Every 5 seconds, get the fact about the previous year
+// When the page loads, start an interval: ✅
+// Every 5 seconds, fetch a fact about a year and show it on the screen in the `#year-history` div✅
+// Start with this year *✅
+// Every 5 seconds, get the fact about the previous year✅
 
-// 4.  All the numbers
-//  When a user clicks the 'All of the Numbers' button fetch facts for one hundred random numbers append a div to `"#all-the-numbers"` to display all numbers and their fact
+// All the numbers
+// When a user clicks the 'All of the Numbers' button fetch facts for one hundred random numbers append a div to `"#all-the-numbers"` to display all numbers and their fact
 
 document.addEventListener('DOMContentLoaded', function () {
   const oneButton = document.querySelector('#number-one');
@@ -27,16 +26,29 @@ document.addEventListener('DOMContentLoaded', function () {
   const pickANumber = document.querySelector('#pick-a-number');
   pickANumber.addEventListener('change', getAnyNumberTrivia);
   setYearTriviaInterval();
-})
+  const allNumbersButton = document.querySelector('#all-numbers-button');
+  allNumbersButton.addEventListener('click', showAllTheNumbers);
+});
+
+
+function getOneFactAboutOne() {
+  const oneFacts = document.querySelector('#one-facts');
+  fetch('http://numbersapi.com/1/trivia')
+    .then(r => r.text())
+    .then(function (parsedTrivia) {
+      console.log(parsedTrivia);
+      oneFacts.innerHTML = parsedTrivia;
+    });
+}
 
 function setYearTriviaInterval() {
   let year = new Date().getFullYear(); // gets current year
   showYearTrivia(year);
   setInterval(function () {
-    year--
+    year--;
     showYearTrivia(year);
 
-  }, 5000)
+  }, 5000);
 }
 
 function showYearTrivia(year) {
@@ -49,16 +61,6 @@ function showYearTrivia(year) {
 
 }
 
-function getOneFactAboutOne() {
-  const oneFacts = document.querySelector('#one-facts');
-  fetch('http://numbersapi.com/1/trivia')
-    .then(r => r.text())
-    .then(function (parsedTrivia) {
-      console.log(parsedTrivia);
-      oneFacts.innerHTML = parsedTrivia;
-    });
-}
-
 function getAnyNumberTrivia(triviaForm) {
   triviaForm.preventDefault();
   console.log('Here!')
@@ -66,7 +68,7 @@ function getAnyNumberTrivia(triviaForm) {
   const input = parseInt(document.querySelector("#pick-a-number").value);
   // validation
   if (isNaN(input)) {
-    triviaDiv.innerHTML = "Please input a valid number"
+    triviaDiv.innerHTML = "please enter a valid number";
   } else {
     fetch(`http://numbersapi.com/${input}/trivia`)
       .then(r => r.text())
@@ -75,4 +77,17 @@ function getAnyNumberTrivia(triviaForm) {
         triviaDiv.innerHTML = parsedTrivia;
       });
   }
+}
+
+function showAllTheNumbers() {
+  const allNumbersDiv = document.querySelector('#all-the-numbers');
+  fetch('http://numbersapi.com/1..100').then(r => r.json())
+    .then(allNumbersData => {
+      let html = '<ul>'
+      for (key in allNumbersData) {
+        html += `<li>${allNumbersData[key]}</li>`
+      }
+      html += '</ul>'
+      allNumbersDiv.innerHTML = html
+    })
 }
